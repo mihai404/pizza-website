120bcd1578998a1487d2a522f38bd88534f75eb4
mihai404 <mihaimaciru105@gmail.com>
2022-11-01 22:31:47 +0200

    m22

---
+++ index.js
@@ -0,0 +1,6 @@
+const modal=document.querySelector("#pizzaComandaModal");
+const btn= document.querySelector("#comandaPizzaButton");
+const btnClose=document.querySelector("#closeModal");
+btn.addEventListener("click", function() {
+ modal;
+});
