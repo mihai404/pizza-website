c6d94543aae5ff890cff298d628e2659a09b63fa
mihai404 <mihaimaciru105@gmail.com>
2022-11-19 07:46:24 +0200

    da

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const modal=document.querySelector("#pizzaComandaModal");
 const btn= document.querySelector("#comandaPizzaButton");
 const btnClose=document.querySelector("#closeModal");
-btn.addEventListener("click", function() {
- 
+btn.addEventListener("onclick", function() { 
+  
 });
-/*getthelook: {
-   	   	      title: 'Obține acest look!',
-   	   	      add: 'Adăugare',
-   	   	      addedToBag: 'Adăugat în coș'
-   	   	    },
-   	 	gdpr: {
-	      	headerText: 'CONFIDENȚIALITATEA ESTE IMPORTANTĂ!',
-   	  		bodyText:'Confidențialitatea datelor tale este importantă pentru noi. De aceea dorim să te informăm că am actualizat Politica de confidențialitate. Pentru a-ți oferi informații mai transparente și mai clare despre modul în care-ți prelucrăm datele cu caracter personal, am împărțit Politica de confidențialitate în mai multe capitole. În funcție de calitatea de abonat, membru sau posesor al unui cont H&amp;M, ți se aplică diferite părți ale politicii. În fiecare capitol, descriem în detaliu ce date cu caracter personal pe care le avem despre tine și cum le folosim. De asemenea, am extins informațiile despre drepturile tale, de exemplu, cum să obții acces la datele proprii și cum ne poți solicita să nu-ți mai utilizăm datele.',
-	      	linkText: 'Notificarea de Confidențialitate este disponibilă aici.',
-	      	buttonText: 'Ok'*/
+/
